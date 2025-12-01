c9609ede4322037ad6b43247db0ed1ab29ad5a84
raisavar <raisavar@users.noreply.github.com>
2025-12-02 01:56:36 +0200

    improve: improve code readability

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 33
 module.exports = API;
+
+
+// API module for FortnightScheduler
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 43
+module.exports = API;
